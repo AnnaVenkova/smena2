031a6a12ee26586256801fe582e7cb30930f9540
AnnaVenkova <vaa5.averton@gmail.com>
2026-08-05 14:31:36 +0500

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration object with necessary keys.

--- firebase-config.js
+++ firebase-config.js
@@ -10,7 +10,14 @@
 // 5. Скопируйте объект firebaseConfig, который покажет Firebase, и вставьте его вместо null ниже.
 // 6. Сохраните файл, закоммитьте и запушьте в GitHub — через минуту сайт обновится.
 
-const FIREBASE_CONFIG = null;
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyALRvBuWHojHwx0nwVy-bkPhkFiyq8LI_k",
+  authDomain: "smena3-975cd.firebaseapp.com",
+  projectId: "smena3-975cd",
+  storageBucket: "smena3-975cd.firebasestorage.app",
+  messagingSenderId: "498368556296",
+  appId: "1:498368556296:web:1231f854420b098191d0bd"
+};
 
 /* Пример заполненной конфигурации (у вас будут другие значения):
 const FIREBASE_CONFIG = {
