31877c9f517e7a8376091562cef88a5e7d0272dd
AnnaVenkova <vaa5.averton@gmail.com>
2026-09-16 12:20:57 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -6,8 +6,8 @@
 // 3. Сохраните, закоммитьте, запушьте.
 
 const SUPABASE_CONFIG = {
-  url: "",       // например https://xxxx.supabase.co
-  anonKey: ""    // anon public key
+  url: "// https://gdroatsdhhxpimsecagp.supabase.co/rest/v1/",       // например https://xxxx.supabase.co
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkcm9hdHNkaGh4cGltc2VjYWdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MTYzNzQsImV4cCI6MjEwNTA5MjM3NH0.fsKBk217kL9jjtskNVcPPKFG6VRi4WNlRM97q6L6Brg"    // anon public key
 };
 
 // Пока url/anonKey пустые — приложение работает локально (без облака).
